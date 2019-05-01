e38db4bd9b6a3ec1c9d15ea6b4f5a2161a440cf4
Neha Upadhyay <upaneha1212@gmail.com>
2019-05-01 19:09:25 +0530

    Adding files for Factory Method Pattern

---
+++ Factory/FactoryDP/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace FactoryMethodDP
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string dbType = Console.ReadLine();
+
+            IDALFactory dalFactoryObj;
+            if (dbType == "SQL")
+                dalFactoryObj = new SQLDALFactory();
+            else
+                dalFactoryObj = new OracleDALFactory();
+
+            IDAL dalObj = dalFactoryObj.CreateDALObject();
+            Console.WriteLine($"Connection String for {dbType} is: {dalObj.GetConnectionString()}");
+        }
+    }
+
+    public interface IDAL
+    {
+        string GetConnectionString();
+    }
+
+    public class SQLDAL : IDAL
+    {
+        public string GetConnectionString()
+        {
+            return "Data Source=myServerAddress;Initial Catalog=myDataBase;Integrated Security=SSPI;\nUser ID=myDomain\\myUsername;Password=myPassword;";
+        }
+    }
+
+    public class OracleDAL : IDAL
+    {
+        public string GetConnectionString()
+        {
+            return "Data Source=MyOracleDB;User Id=myUsername;Password=myPassword;\nIntegrated Security=no;";
+        }
+    }
+
+    public interface IDALFactory
+    {
+        IDAL CreateDALObject();
+    }
+
+    public class SQLDALFactory : IDALFactory
+    {
+        public IDAL CreateDALObject()
+        {
+            return new SQLDAL();
+        }
+    }
+
+    public class OracleDALFactory : IDALFactory
+    {
+        public IDAL CreateDALObject()
+        {
+            return new OracleDAL();
+        }
+    }
+}
