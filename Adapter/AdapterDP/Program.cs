34c11f94ec1c81f36418f363ad94c1079eeaa57e
Neha Upadhyay <upaneha1212@gmail.com>
2019-05-01 20:06:45 +0530

    Adding files for adapter design pattern

---
+++ Adapter/AdapterDP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdapterDP
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            IShoppingSiteTarget shopping = new Adapter();
+            foreach (var item in shopping.GetProducts())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public interface IShoppingSiteTarget
+    {
+        List<string> GetProducts();
+    }
+
+    public class VendorAdaptee
+    {
+        public List<string> Products => new List<string> { "Television", "Books", "Cosmetics"};
+    }
+
+    public class Adapter : IShoppingSiteTarget
+    {
+        public List<string> GetProducts()
+        {
+            VendorAdaptee adaptee = new VendorAdaptee();
+            return adaptee.Products;
+        }
+    }
+}
